8f4f23c73b360fe1fd0de94934a7bc36e4552dfa
Lucas Krul <l3k.scorp@gmail.com>
2025-10-16 20:51:34 -0300

    test: change strategy to use late property

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 class NextEventPlayer {
   final String id;
   final String name;
+  late final String initials;
   final String? photo;
   final String? position;
   final bool isComfirmed;
@@ -15,9 +16,11 @@ class NextEventPlayer {
     this.photo,
     this.position,
     this.confirmationDate,
-  });
+  }) {
+    initials = _getInitials();
+  }
 
-  String getInitials() {
+  String _getInitials() {
     final names = name.split(' ');
     final firstChar = names.first[0];
     final lastChar = names.last[0];
@@ -26,13 +29,13 @@ class NextEventPlayer {
 }
 
 void main() {
-  NextEventPlayer makeSut(String name) =>
-      NextEventPlayer(id: '', name: name, isComfirmed: true);
+  String initialsOf(String name) =>
+      NextEventPlayer(id: '', name: name, isComfirmed: true).initials;
   test('should return the first letter of the first and last name', () {
-    expect(makeSut('John Doe').getInitials(), 'JD');
+    expect(initialsOf('John Doe'), 'JD');
 
-    expect(makeSut('Jane Smith').getInitials(), 'JS');
+    expect(initialsOf('Jane Smith'), 'JS');
 
-    expect(makeSut('Jack Daniels Jr').getInitials(), 'JJ');
+    expect(initialsOf('Jack Daniels Jr'), 'JJ');
   });
 }
