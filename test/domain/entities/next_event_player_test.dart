cbb4c47d83384db6868bd021c255c69844889862
Lucas Krul <l3k.scorp@gmail.com>
2025-10-17 19:45:11 -0300

    test: change strategy to use factory constructor

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -3,34 +3,51 @@ import 'package:flutter_test/flutter_test.dart';
 class NextEventPlayer {
   final String id;
   final String name;
-  late final String initials;
+  final String initials;
   final String? photo;
   final String? position;
-  final bool isComfirmed;
+  final bool isConfirmed;
   final DateTime? confirmationDate;
 
-  NextEventPlayer({
+  const NextEventPlayer._({
     required this.id,
     required this.name,
-    required this.isComfirmed,
+    required this.initials,
+    required this.isConfirmed,
     this.photo,
     this.position,
     this.confirmationDate,
-  }) {
-    initials = _getInitials();
-  }
+  });
 
-  String _getInitials() {
-    final names = name.split(' ');
-    final firstChar = names.first[0];
-    final lastChar = names.last[0];
-    return '$firstChar$lastChar'.toUpperCase();
+  factory NextEventPlayer({
+    required String id,
+    required String name,
+    required bool isConfirmed,
+    String? photo,
+    String? position,
+    DateTime? confirmationDate,
+  }) => NextEventPlayer._(
+    id: id,
+    name: name,
+    photo: photo,
+    position: position,
+    initials: _getInitials(name),
+    isConfirmed: isConfirmed,
+    confirmationDate: confirmationDate,
+  );
+
+  static String _getInitials(String name) {
+    final names = name.toUpperCase().trim().split(' ');
+    final firstChar = names.first.split('').firstOrNull ?? '-';
+    final lastChar =
+        names.last.split('').elementAtOrNull(names.length == 1 ? 1 : 0) ?? '';
+    return '$firstChar$lastChar';
   }
 }
 
 void main() {
   String initialsOf(String name) =>
-      NextEventPlayer(id: '', name: name, isComfirmed: true).initials;
+      NextEventPlayer(id: '', name: name, isConfirmed: true).initials;
   test('should return the first letter of the first and last name', () {
     expect(initialsOf('John Doe'), 'JD');
 
