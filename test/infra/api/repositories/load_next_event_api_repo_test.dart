f6ceaf3d8ffe33ad78f3ce20a4a7c22c48ecc62c
Lucas Krul <l3k.scorp@gmail.com>
2025-10-23 19:34:01 -0300

    test: ensure LoadNextEventApiRepository rethrows on HttpClient error

--- test/infra/api/repositories/load_next_event_api_repo_test.dart
+++ test/infra/api/repositories/load_next_event_api_repo_test.dart
@@ -44,6 +44,7 @@ class HttpGetClientSpy implements HttpGetClient {
   int callsCount = 0;
   Map<String, String>? params;
   dynamic response;
+  Error? error;
 
   @override
   Future<dynamic> get({
@@ -53,6 +54,9 @@ class HttpGetClientSpy implements HttpGetClient {
     this.url = url;
     this.params = params;
     callsCount++;
+    if (error != null) {
+      throw error!;
+    }
     return response;
   }
 }
@@ -106,5 +110,12 @@ void main() {
     expect(event.players[1].photo, 'photo 2');
     expect(event.players[1].confirmationDate, DateTime(2025, 8, 29, 09, 45));
     expect(event.players[1].isConfirmed, false);
+  });
+
+  test('should rethrow on error', () async {
+    final error = Error();
+    httpClient.error = error;
+    final future = sut.loadNextEvent(groupId: groupId);
+    expect(future, throwsA(error));
   });
 }
