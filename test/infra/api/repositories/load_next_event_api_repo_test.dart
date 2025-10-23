dbd9544d834954f386186340a39bcf64d6c3005c
Lucas Krul <l3k.scorp@gmail.com>
2025-10-23 20:23:44 -0300

    test: create type alias to improve readability

--- test/infra/api/repositories/load_next_event_api_repo_test.dart
+++ test/infra/api/repositories/load_next_event_api_repo_test.dart
@@ -5,6 +5,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../../../helpers/fakes.dart';
 
+typedef Json = Map<String, dynamic>;
+typedef JsonArr = List<Json>;
+
 class LoadNextEventApiRepository implements LoadNextEventRepository {
   final HttpGetClient httpClient;
   final String url;
@@ -13,7 +16,7 @@ class LoadNextEventApiRepository implements LoadNextEventRepository {
 
   @override
   Future<NextEvent> loadNextEvent({required String groupId}) async {
-    final json = await httpClient.get<Map<String, dynamic>>(
+    final json = await httpClient.get<Json>(
       url: url,
       params: {'groupId': groupId},
     );
@@ -22,7 +25,7 @@ class LoadNextEventApiRepository implements LoadNextEventRepository {
 }
 
 class NextEventMapper {
-  static NextEvent toObject(Map<String, dynamic> json) => NextEvent(
+  static NextEvent toObject(Json json) => NextEvent(
     groupName: json['groupName'],
     date: DateTime.parse(json['date']),
     players: NextEventPlayerMapper.toList(json['players']),
@@ -30,9 +33,9 @@ class NextEventMapper {
 }
 
 class NextEventPlayerMapper {
-  static List<NextEventPlayer> toList(List<Map<String, dynamic>> arr) =>
+  static List<NextEventPlayer> toList(JsonArr arr) =>
       arr.map(NextEventPlayerMapper.toObject).toList();
-  static NextEventPlayer toObject(Map<String, dynamic> json) => NextEventPlayer(
+  static NextEventPlayer toObject(Json json) => NextEventPlayer(
     id: json['id'],
     name: json['name'],
     position: json['position'],
@@ -43,18 +46,18 @@ class NextEventPlayerMapper {
 }
 
 abstract class HttpGetClient {
-  Future<T> get<T>({required String url, Map<String, String>? params});
+  Future<T> get<T>({required String url, Json? params});
 }
 
 class HttpGetClientSpy implements HttpGetClient {
   String? url;
   int callsCount = 0;
-  Map<String, String>? params;
+  Json? params;
   dynamic response;
   Error? error;
 
   @override
-  Future<T> get<T>({required String url, Map<String, String>? params}) async {
+  Future<T> get<T>({required String url, Json? params}) async {
     this.url = url;
     this.params = params;
     callsCount++;
