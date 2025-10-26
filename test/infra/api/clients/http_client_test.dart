8a49f8ece374a8a38566eb59afc27a096af76421
Lucas Krul <l3k.scorp@gmail.com>
2025-10-26 16:04:41 -0300

    test: ensure HttpCLient throws SessionExpiredError on 401

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -26,6 +26,8 @@ class HttpClient {
     switch (response.statusCode) {
       case 200:
         break;
+      case 401:
+        throw DomainError.sessionExpired;
       default:
         throw DomainError.unexpected;
     }
@@ -138,6 +140,12 @@ void main() {
       client.simulateBadRequestError();
       final future = sut.get(url: url);
       expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('should throw SessionExpiredError on 401', () async {
+      client.simulateUnauthorizedError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.sessionExpired));
     });
   });
 }
