e6781075eb14973e95fd18aeb1e88bd2509c3a38
Lucas Krul <l3k.scorp@gmail.com>
2025-10-26 19:02:47 -0300

    test: move HttpAdapter to its own file

---
+++ lib/infra/api/adapters/http_adapter.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:advanced_flutter/domain/entites/domain_error.dart';
+import 'package:dartx/dartx.dart';
+import 'package:http/http.dart';
+
+class HttpAdapter {
+  final Client client;
+  HttpAdapter({required this.client});
+
+  Future<dynamic> get({
+    required String url,
+    Map<String, String>? headers,
+    Map<String, String?>? params,
+    Map<String, String>? queryString,
+  }) async {
+    final allHeaders = (headers ?? {})
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+      });
+    final uri = _buildUri(url: url, params: params, queryString: queryString);
+    final response = await client.get(uri, headers: allHeaders);
+    switch (response.statusCode) {
+      case 200:
+        {
+          if (response.body.isEmpty) return null;
+          return jsonDecode(response.body);
+        }
+      case 204:
+        return null;
+      case 401:
+        throw DomainError.sessionExpired;
+      default:
+        throw DomainError.unexpected;
+    }
+  }
+
+  Uri _buildUri({
+    required String url,
+    Map<String, String?>? params,
+    Map<String, String>? queryString,
+  }) {
+    url =
+        params?.keys
+            .fold(
+              url,
+              (result, key) => result.replaceFirst(':$key', params[key] ?? ''),
+            )
+            .removeSuffix('/') ??
+        url;
+    url =
+        queryString?.keys
+            .fold('$url?', (result, key) => '$result$key=${queryString[key]}&')
+            .removeSuffix('&') ??
+        url;
+    return Uri.parse(url);
+  }
+}
