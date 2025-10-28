fae2bff0cef69a1d57e62cbb19ff381ce34d8a9e
Lucas Krul <l3k.scorp@gmail.com>
2025-10-28 19:26:49 -0300

    test: apply test in PlayerPhoto

---
+++ test/ui/components/player_photo_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+class PlayerPhoto extends StatelessWidget {
+  final String initials;
+  final String? photo;
+
+  const PlayerPhoto({required this.initials, this.photo, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      foregroundImage: photo != null ? NetworkImage(photo!) : null,
+      child: photo == null ? Text(initials) : null,
+    );
+  }
+}
+
+void main() {
+  testWidgets('should present initials when there is no photo', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(home: PlayerPhoto(initials: 'RO', photo: null)),
+    );
+    expect(find.text('RO'), findsOneWidget);
+  });
+
+  testWidgets('should hide initials when there is photo', (tester) async {
+    mockNetworkImagesFor(() async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: PlayerPhoto(
+            initials: 'RO',
+            photo: 'http://example.com/photo.jpg',
+          ),
+        ),
+      );
+      expect(find.text('RO'), findsNothing);
+    });
+  });
+}
