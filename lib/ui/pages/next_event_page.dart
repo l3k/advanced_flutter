8eb4af875a7d2b244e0977a509b5c71bcbac14bb
Lucas Krul <l3k.scorp@gmail.com>
2025-10-28 20:14:56 -0300

    test: ensure NextEventPage render error message on load error

--- lib/ui/pages/next_event_page.dart
+++ lib/ui/pages/next_event_page.dart
@@ -25,6 +25,13 @@ class _NextEventPageState extends State<NextEventPage> {
     super.initState();
   }
 
+  Widget buildErrorLayout() => Column(
+    children: [
+      const Text('Algo errado aconteceu, tente novamente.'),
+      ElevatedButton(onPressed: () {}, child: const Text('Recarregar')),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +41,7 @@ class _NextEventPageState extends State<NextEventPage> {
           if (snapshot.connectionState != ConnectionState.active) {
             return const CircularProgressIndicator();
           }
-          if (snapshot.hasError) return const SizedBox();
+          if (snapshot.hasError) return buildErrorLayout();
           final viewData = snapshot.data;
           return ListView(
             children: [
