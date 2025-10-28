5fd7828283570a6b104651dfb2c017c3eb600f58
Lucas Krul <l3k.scorp@gmail.com>
2025-10-28 16:52:10 -0300

    test: handle defender, midfielder and forward positions

--- test/ui/components/player_position_test.dart
+++ test/ui/components/player_position_test.dart
@@ -28,6 +28,27 @@ void main() {
     expect(find.text('Goleiro'), findsOneWidget);
   });
 
+  testWidgets("should handle defender position", (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(home: PlayerPosition(position: 'defender')),
+    );
+    expect(find.text('Zagueiro'), findsOneWidget);
+  });
+
+  testWidgets("should handle midfielder position", (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(home: PlayerPosition(position: 'midfielder')),
+    );
+    expect(find.text('Meio-campista'), findsOneWidget);
+  });
+
+  testWidgets("should handle forward position", (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(home: PlayerPosition(position: 'forward')),
+    );
+    expect(find.text('Atacante'), findsOneWidget);
+  });
+
   testWidgets("should handle positionless", (tester) async {
     await tester.pumpWidget(MaterialApp(home: PlayerPosition(position: null)));
     expect(find.text('Gandula'), findsOneWidget);
