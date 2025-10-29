b7be41ca7741189c34710ab6a4209c2da0292701
Lucas Krul <l3k.scorp@gmail.com>
2025-10-29 20:29:51 -0300

    test: change reload click to call load with isReload param

--- lib/ui/pages/next_event_page.dart
+++ lib/ui/pages/next_event_page.dart
@@ -40,7 +40,10 @@ class _NextEventPageState extends State<NextEventPage> {
       const Text('Algo errado aconteceu, tente novamente.'),
       ElevatedButton(
         onPressed: () {
-          widget.presenter.reloadNextEvent(groupId: widget.groupId);
+          widget.presenter.loadNextEvent(
+            groupId: widget.groupId,
+            isReload: true,
+          );
         },
         child: const Text('Recarregar'),
       ),
